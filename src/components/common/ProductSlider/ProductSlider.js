d5fa65b132f56846a9837faa4d2f5ea5805b99ee
Maciej Michalik <halniak24@gmail.com>
2021-11-11 19:59:49 +0100

    Add sliders in promoted

--- src/components/common/ProductSlider/ProductSlider.js
+++ src/components/common/ProductSlider/ProductSlider.js
@@ -1,28 +1,100 @@
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-import ProductBox from '../../common/ProductBox/ProductBoxContainer'
+import Button from '../Button/Button';
+import ProductBox from '../../common/ProductBox/ProductBoxContainer';
+import PromoCountdownTimer from '../PromoCountdownTimer/PromoCountdownTimer';
+import PropTypes from 'prop-types';
 import React from 'react';
 import Slider from 'react-slick';
-import styles from './ProductSlider.module.scss'
+import styles from './ProductSlider.module.scss';
 
-export default function ProductSlider({ promotedProducts }) {
-  const settings = {
-    dots: true,
-    infinite: true,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    appendDots: dots => <ul id='xyz'>{dots}</ul>,
+class ProductSlider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      settings: {
+        fade: true,
+        dotsClass: `slick-dots ${styles.customdots}`,
+        afterChange: index => {
+          const activeDot = document.querySelectorAll('.slick-dots > li')[index];
+          activeDot.classList.add(styles.customdots__active);
+          activeDot.classList.remove(`slick-active`);
+        },
+      },
+    };
+  }
+
+  static propTypes = {
+    settings: PropTypes.object,
+    promotedProducts: PropTypes.array,
   };
 
-  return (
-    <Slider {...settings}>
-      {promotedProducts.map(product =>
-      <div>
-          {/* <PromoCountdownTimer /> */}
-        <ProductBox key={product.id} {...product} />
-      </div>
-      )}
-    </Slider>
-  );
+  pause = () => {
+    this.slider.slickPause();
+    setTimeout(() => {
+      this.slider.slickPlay();
+    }, this.props.settings.pauseTime);
+  };
+
+  next = event => {
+    event.preventDefault();
+    this.slider.slickNext();
+  };
+
+  prev = event => {
+    event.preventDefault();
+    this.slider.slickPrev();
+  };
+
+  render() {
+    const { settings, promotedProducts } = this.props;
+    if (promotedProducts) {
+      return (
+        <div onClick={() => this.pause()}>
+          <div className={styles.customdots__bg} />
+          <Slider
+            ref={slider => (this.slider = slider)}
+            {...this.state.settings}
+            {...settings}
+          >
+            {promotedProducts.map(product => (
+              <div key={product.id}>
+                {settings.promoCountdown ? <PromoCountdownTimer /> : null}
+                <ProductBox {...product} />
+              </div>
+            ))}
+          </Slider>
+          <div className={settings.arrowButtons ? '' : 'd-none'}>
+            <div className={styles.shadowWrapper}></div>
+            <div className={styles.shadowTitle}>
+              INDOOR <span>FURNITURE</span>
+              <div className={styles.shadowSubtitle}>
+                SAVE UP TO 50% OF ALL FURNITURE
+              </div>
+            </div>
+            <div className={styles.centerButton}>
+              <Button variant='big'>SHOP NOW</Button>
+            </div>
+            <div className={styles.shadowButtonsWrapper}>
+              <div className='row'>
+                <div className={styles.buttonLong}>
+                  <Button onClick={this.prev} variant='long'>
+                    {'<'}
+                  </Button>
+                </div>
+                <div className={styles.buttonLong}>
+                  <Button onClick={this.next} variant='long'>
+                    {'>'}
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
 }
+
+export default ProductSlider;
--- src/components/features/Promoted/Promoted.js
+++ src/components/features/Promoted/Promoted.js
@@ -1,79 +1,64 @@
-import {
-  faExchangeAlt,
-  faEye,
-  faShoppingBasket,
-  faStar,
-} from '@fortawesome/free-solid-svg-icons';
-import { faHeart, faStar as farStar } from '@fortawesome/free-regular-svg-icons';
-
-import Button from '../../common/Button/Button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import ProductBox from '../../common/ProductBox/ProductBoxContainer';
-import ProductSlider from '../../common/ProductSlider/ProductSlider'
-import PromoCountdownTimer from '../../common/PromoCountdownTimer/PromoCountdownTimer'
+import ProductSlider from '../../common/ProductSlider/ProductSlider';
 import PropTypes from 'prop-types';
 import React from 'react';
-import initialState from '../../../redux/initialState';
 import styles from './Promoted.module.scss';
 
-const Promoted = ({ promotedProducts }) => {
+class Promoted extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      settings: {
+        sliderLeft: {
+          dots: true,
+          infinite: true,
+          autoplay: true,
+          autoplaySpeed: 3000,
+          pauseTime: 10000,
+          arrowButtons: false,
+          promoCountdown: true,
+        },
+        sliderRight: {
+          dots: false,
+          autoplay: false,
+          pauseTime: 0,
+          arrowButtons: true,
+          promoCountdown: false,
+        },
+      },
+    };
+  }
 
-  return (
-    <div className={styles.root}>
-      <div className='container'>
-        <div className='row'>
-          <div className={'col-4 ' + styles.left}>
-            <div className={styles.topper}>
-              <div className={'col-auto ' + styles.hotDeals}>
-                <h3>HOT DEALS</h3>
-              </div>
-              <div className={'col-4 ' + styles.dots}>
-                <ul>
-                  <li>
-                    <a></a>
-                  </li>
-                  <li>
-                    <a></a>
-                  </li>
-                  <li>
-                    <a></a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <ProductSlider promotedProducts={promotedProducts} />
-          </div>
-          <div className={'col-8 ' + styles.right}>
-            <div className={styles.imgWrapper}>
-              <img src={initialState.products[12].image} alt='right side image' />
-            </div>
-            <div className={styles.shadowWrapper}></div>
-            <div className={styles.shadowTitle}>
-              INDOOR <span>FURNITURE</span>
-              <div className={styles.shadowSubtitle}>SAVE UP TO 50% OF ALL FURNITURE</div>
-            </div>
-            <div className={styles.centerButton}>
-              <Button variant='big'>SHOP NOW</Button>
-            </div>
-            <div className={styles.shadowButtonsWrapper}>
-              <div className='row'>
-                <div className={styles.buttonLong}>
-                  <Button variant='long'>{'<'}</Button>
-                </div>
-                <div className={styles.buttonLong}>
-                  <Button variant='long'>{'>'}</Button>
+  static propTypes = {
+    promotedProducts: PropTypes.array,
+  };
+
+  render() {
+    return (
+      <div className={styles.root}>
+        <div className='container'>
+          <div className='row'>
+            <div className={'col-4 ' + styles.left}>
+              <div className={styles.topper}>
+                <div className={'col-auto ' + styles.hotDeals}>
+                  <h3>HOT DEALS</h3>
                 </div>
               </div>
+              <ProductSlider
+                promotedProducts={this.props.promotedProducts}
+                settings={this.state.settings.sliderLeft}
+              />
+            </div>
+            <div className={'col-8 ' + styles.right}>
+              <ProductSlider
+                promotedProducts={this.props.promotedProducts}
+                settings={this.state.settings.sliderRight}
+              />
             </div>
           </div>
         </div>
       </div>
-    </div>
-  )
-};
-
-Promoted.propTypes = {
-  promotedProducts: PropTypes.array
+    );
+  }
 }
 
 export default Promoted;
