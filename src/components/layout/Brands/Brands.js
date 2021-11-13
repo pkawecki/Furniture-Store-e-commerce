6ec64baa363009a17b797f690b643024c93680a0
Paulina Podraza <paulina.podraza92@gmail.com>
2021-11-13 13:17:12 +0100

    Add functionality for the 'Brands' component

--- src/components/layout/Brands/Brands.js
+++ src/components/layout/Brands/Brands.js
@@ -8,7 +8,36 @@ import BrandsSlider from '../../features/BrandsSlider/BrandsSlider';
 
 const Brands = ({ brands }) => {
   const [brand] = useState(6);
-  const [activePage] = useState(0);
+  const [activePage, ActivePage] = useState(0);
+
+  let newRwd = 0;
+  const rwd = window.innerWidth;
+  if (rwd >= 576 && rwd < 768) {
+    newRwd = 4;
+  } else if (rwd >= 0 && rwd < 576) {
+    newRwd = 2;
+  } else {
+    newRwd = 6;
+  }
+
+  const leftAction = e => {
+    e.preventDefault();
+
+    ActivePage(activePage === 0 ? brands.length / brand - 1 : Math.ceil(brands.length / newRwd));
+  };
+
+  const rightAction = e => {
+    e.preventDefault();
+
+    ActivePage(activePage === brands.length / brand - 1 ? 0 : activePage + 1);
+    // if(newActivePage === 0) {
+    //   newActivePage++;
+    // } else if (newActivePage >= pagesCount-1)
+    // { newActivePage = 0;
+    // } else if (newActivePage >= 0) {
+    //   newActivePage++;
+    // }
+  };
 
   return (
     <div className={styles.root}>
@@ -19,13 +48,15 @@ const Brands = ({ brands }) => {
           }
         >
           <div className={`col-1`}>
-            <Button className={styles.button} variant='carousel'>
+            <Button className={styles.button} variant='carousel'  onClick={leftAction}>
               <FontAwesomeIcon icon={faChevronLeft}></FontAwesomeIcon>
             </Button>
           </div>
           <div className='col-10'>
             <div className='row justify-content-between'>
-              {brands.slice(activePage * brand, (activePage + 1) * brand).map(item => (
+              {/* {brands.slice(newActivePage * newRwd, (newActivePage + 1) * newRwd).map(item => (
+                <div key={item.id} className='col-2'> */}
+              {brands.slice(activePage * newRwd, (activePage + 1) * newRwd).map(item => (
                 <div key={item.id} className='col-2'>
                   <BrandsSlider {...item} />
                 </div>
@@ -33,7 +64,7 @@ const Brands = ({ brands }) => {
             </div>
           </div>
           <div className={`col-1`}>
-            <Button className={styles.button} variant='carousel'>
+            <Button className={styles.button} variant='carousel'  onClick={rightAction}>
               <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
             </Button>
           </div>
