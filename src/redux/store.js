1f4642bf0f82d26ec59f00b07d6c7f52372b5ac2
michalik-maciej <halniak24@gmail.com>
2021-11-15 20:09:39 +0100

    Update store.js
    naprawiam swój niechcący merge, który spowodował podwójny import brandsRedux.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,7 +7,6 @@ import feedbacksReducer from './feedbacksRedux';
 import initialState from './initialState';
 import modeReducer from './modeRedux';
 import productsReducer from './productsRedux';
-import brandsReducer from './brandsRedux';
 import compareReducer from './compareRedux';
 
 
