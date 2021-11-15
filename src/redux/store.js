dce57e331f1cadbc31ec9143dbbe8d96b2a8f097
Maciej Michalik <halniak24@gmail.com>
2021-11-15 21:46:01 +0100

    first

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,19 +3,18 @@ import { combineReducers, createStore } from 'redux';
 import brandsReducer from './brandsRedux';
 import cartReducer from './cartRedux';
 import categoriesReducer from './categoriesRedux';
+import compareReducer from './compareRedux';
 import feedbacksReducer from './feedbacksRedux';
 import initialState from './initialState';
 import modeReducer from './modeRedux';
 import productsReducer from './productsRedux';
-import brandsReducer from './brandsRedux';
-import compareReducer from './compareRedux';
-
 
 // define reducers
 const reducers = {
   cart: cartReducer,
   categories: categoriesReducer,
   products: productsReducer,
+  compare: compareReducer,
   mode: modeReducer,
   brands: brandsReducer,
   feedbacks: feedbacksReducer,
