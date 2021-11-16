8ca5cf5bdbfe2a01c6a5748cb6b47822d520e1c7
Przemek Kawecki <przemek.kawecki@gmail.com>
2021-11-16 13:37:41 +0100

    Chatbox classList error fixed, company logo image rendering minor fix

--- src/components/views/ProductPage/ProductPage.js
+++ src/components/views/ProductPage/ProductPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../../features/Banner/Banner';
+import Chatbox from '../../features/Chatbox/Chatbox';
 import NavBar from '../../features/NavBar/NavBar';
 // import PropTypes from 'prop-types';
 import styles from './ProductPage.module.scss';
 
 const ProductPage = () => (
   <div className={styles.root}>
+    <Chatbox />
     <Banner />
     <NavBar />
   </div>
