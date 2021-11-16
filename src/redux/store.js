ec592d5ebee98a5dd47bfd0474621cb48c7a443d
Paulina Podraza <80419830+PaulinaPodraza@users.noreply.github.com>
2021-11-16 20:58:17 +0100

    Fix with old price in component 'Gallery'

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,6 +6,7 @@ import initialState from './initialState';
 import modeReducer from './modeRedux';
 import productsReducer from './productsRedux';
 import brandsReducer from './brandsRedux';
+import compareReducer from './compareRedux';
 
 // define reducers
 const reducers = {
