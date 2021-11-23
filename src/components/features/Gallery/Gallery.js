81acb08e45a90d57c7afc98b5c38940895c0d09a
Paulina Podraza <80419830+PaulinaPodraza@users.noreply.github.com>
2021-11-23 22:09:33 +0100

    Add gallery functionality

--- src/components/features/Gallery/Gallery.js
+++ src/components/features/Gallery/Gallery.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import styles from './Gallery.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faStar, faHeart, faExchangeAlt, faEye, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
@@ -6,6 +8,8 @@ import PropTypes from 'prop-types';
 import Button from '../../common/Button/Button';
 import ReactTooltip from 'react-tooltip';
 import initialState from '../../../redux/initialState';
+import Slider from 'react-slick';
+
 class Gallery extends React.Component {
   state = {
     activePage: 0,
@@ -28,36 +32,33 @@ class Gallery extends React.Component {
     }
   }
 
-  handlePageChange(newPage) {
-    this.setState({
-      activePage: newPage,
-      activeFade: true,
-    });
-    if (this.state.activeFade === false) {
-      setTimeout(
-        function() {
-          this.setState({ activeFade: false });
-        }.bind(this),
-        1000
-      );
-    }
-  }
-
-  next = event => {
-    event.preventDefault();
-    this.slider.slickNext();
-    this.pause();
-  };
 
   render() {
-    const { products} = this.props;
-    const { activeCategory } = this.state;
+    const { products } = this.props;
+    const { activeCategory} = this.state;
     const categories = [
       { id: 'featured', name: 'FEATURED' },
-      { id: 'topSeller', name: 'TOP SELLER' },
-      { id: 'saleOff', name: 'SALE OFF' },
-      { id: 'topRated', name: 'TOP RATED' },
+      { id: 'topSeller', name: 'TOP SELLER'},
+      { id: 'saleOff', name: 'SALE OFF'},
+      { id: 'topRated', name: 'TOP RATED'},
     ];
+    const settings = {
+      customPaging: function () {
+        return (
+          <a className={styles.boximage}>
+            {/* <img src={`${initialState.products.image}`} alt='' /> */}
+            <img src='https://i.ibb.co/p3jjsP6/chairs.jpg' alt='' />
+          </a>
+        );
+      },
+      dots: true,
+      dotsClass: 'slick-dots slick-thumb',
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    };
+
     return (
       <div className={styles.root}>
         <div className='container'>
@@ -66,7 +67,6 @@ class Gallery extends React.Component {
               <div className={styles.heading}>
                 <h3>Furniture gallery</h3>
               </div>
-
               {/* Gallery tabs */}
               <div className={styles.menu}>
                 <ul>
@@ -75,9 +75,8 @@ class Gallery extends React.Component {
                       {/* eslint-disable-next-line */}
                       <span
                         className={
-                          item.id === activeCategory ? styles.active : undefined
-                        }
-                        onClick={() => this.handleCategoryChange(item.id)}
+                          item.id === activeCategory ? styles.active : null }
+                        onClick={event => this.handleCategoryChange(event, item.id, categories.catgory)}
                       >
                         {item.name}
                       </span>
@@ -88,81 +87,66 @@ class Gallery extends React.Component {
 
               {/*Left side of the gallery*/}
               <div className={styles.product}>
-                <img
-                  src={initialState.products[0].image}
-                  alt='product-1'
-                />
                 <div className={styles.buttons}>
                   <Button data-for='btn' data-tip='Add to favorites' variant='outline' className={styles.icon}>
                     <FontAwesomeIcon icon={faHeart}>Add to favorites</FontAwesomeIcon>
                   </Button>
                   <ReactTooltip id='btn' place='right' getContent={(dataTip) => `${dataTip}`}/>
-
                   <Button data-for='btn' data-tip='Add to compare' variant='outline' className={styles.icon}>
                     <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
                   </Button>
                   <ReactTooltip id='btn' place='right' getContent={(dataTip) => `${dataTip}`}/>
-
                   <Button data-for='btn' data-tip='View details' variant='outline' className={styles.icon}>
                     <FontAwesomeIcon icon={faEye}>View details</FontAwesomeIcon>
                   </Button>
                   <ReactTooltip id='btn' place='right' getContent={(dataTip) => `${dataTip}`}/>
-
                   <Button data-for='btn' data-tip='Add to basket' variant='outline' className={styles.icon}>
                     <FontAwesomeIcon icon={faShoppingBasket}>Add to basket</FontAwesomeIcon>
                   </Button>
                   <ReactTooltip id='btn' place='right' getContent={(dataTip) => `${dataTip}`}/>
                 </div>
-
                 {/*Rating*/}
-                <div className={styles.content}>
-                  <div className={styles.cornerTopLeft} />
-                  <h5>{products[0].name}</h5>
-                  <div className={styles.stars}>
-                    <span>
-                      <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
-                      <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
-                      <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
-                      <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
-                      <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
-                    </span>
-                  </div>
-                  <div className={styles.cornerBottomRight} />
-                  <div className={styles.price}>
-                    <h3>${products[0].price}</h3>
-                    <h3>
-                      <s>${products[0].oldPrice}</s>
-                    </h3>
-                  </div>
-                </div>
-              </div>
 
-              {/*Slider*/}
+              </div>
               <div className={styles.slider}>
-                <div className={styles.preview}>
-                  <p>{'<'}</p>
-                </div>
-                <div className={styles.thumbnails}>
+                <Slider {...settings}>
                   {products.slice(0, 6).map((product, index) => (
-                    <div key={product.id}>
+                    <div key={product.id}><div className={styles.box}>
                       <img
-                        src={initialState.products[2].image}
+                        src={product.image}
                         alt=''
+                        onClick={() => this.handleProductChange(product)}
                         className={
                           index === 0
                             ? styles.thumbnail + ' ' + styles.active
                             : styles.thumbnail
                         }
-                      />
+                      /></div>
+                    <div className={styles.content}>
+                      <div className={styles.cornerTopLeft} />
+                      <h5>{product.name}</h5>
+                      <div className={styles.stars}>
+                        <span>
+                          <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                          <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                          <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                          <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                          <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                        </span>
+                      </div>
+                      <div className={styles.cornerBottomRight} />
+                      <div className={styles.price}>
+                        <h3>${product.price}</h3>
+                        <h3>
+                          <s>${product.oldPrice}</s>
+                        </h3>
+                      </div>
+                    </div>
                     </div>
                   ))}
-                </div>
-                <div className={styles.next}>
-                  <Button variant='carousel' onClick={this.next}>{'>'}</Button>
-                </div>
+                </Slider>
               </div>
             </div>
-
             {/*Right side of the gallery*/}
             <div className={'col-6 ' + styles.picture}>
               <img
@@ -183,13 +167,14 @@ class Gallery extends React.Component {
     );
   }
 }
-
 Gallery.propTypes = {
   products: PropTypes.arrayOf(
     PropTypes.shape({
       name: PropTypes.string,
       price: PropTypes.number,
       oldPrice: PropTypes.number,
+      image: PropTypes.string,
+      category: PropTypes.string,
     })
   ),
   categories: PropTypes.arrayOf(
